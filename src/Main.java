8ee9d386bf86615e487342d022fb89ec9e386113
Yulia Lener <yulias.docs@gmail.com>
2024-02-06 20:24:59 +0700

    main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
         int eaters = 5; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
@@ -13,7 +10,7 @@ public class Main {
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров(а) воды");
-        System.out.println((картофель / едоки) + " картофелин(а/ы)");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ки/ка) специй");
 
